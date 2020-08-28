387076b585877799517944d5767cd5a7815722b5
Dayana <dsespitia@ucundinamarca.edu.co>
2020-08-27 22:17:13 -0500

    Cambio 8

--- TablaMultiplicacion.java
+++ TablaMultiplicacion.java
@@ -18,6 +18,7 @@ public static void main(String[] args){
 		System.out.print ("Cambio 5");
 		System.out.print ("Cambio 6"); 
 		System.out.print ("Cambio 7");
+		System.out.print ("Cambio 8");
 		
 
 
